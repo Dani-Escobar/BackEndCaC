0ab7f1515df9d62d7a8b94e01bc3bb8fb3add0a7
Dani-Escobar <102489881+Dani-Escobar@users.noreply.github.com>
2023-07-12 18:07:28 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 document.getElementById("header").innerHTML=` <nav class="navbar navbar-expand-sm
 navbar-light bg-light">
 <div class="container">
-<a class="navbar-brand" href="/home/dani/Downloads/TFI/nuevo/templates/productos.html">Consultorio Odontológico</a>
+<a class="navbar-brand" href="index.html">Consultorio Odontológico</a>
 
 </div>
 </nav>
