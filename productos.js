3b858e818bf542abd2dbe3b4ac4cd721f66f7a31
Dani-Escobar <102489881+Dani-Escobar@users.noreply.github.com>
2023-07-12 18:10:04 -0300

    Update productos.js

--- productos.js
+++ productos.js
@@ -58,7 +58,7 @@ redirect: 'follow'
 fetch(this.url, options)
 .then(function () {
 alert("Registro grabado")
-window.location.href = "/home/dani/Downloads/TFI/nuevo/templates/productos.html";
+window.location.href = "index.html";
 })
 .catch(err => {
 console.error(err);
