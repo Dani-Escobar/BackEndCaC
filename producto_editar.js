ea04e5a29afa8ab17e6b4b3acd76c5e912c394b5
Dani-Escobar <102489881+Dani-Escobar@users.noreply.github.com>
2023-07-12 17:59:31 -0300

    Add files via upload

---
+++ producto_editar.js
@@ -0,0 +1,63 @@
+console.log(location.search) // lee los argumentos pasados a este formulario
+var id=location.search.substr(4)
+console.log(id)
+const { createApp } = Vue
+createApp({
+data() {
+return {
+id:0,
+nombre:"",
+imagen:"",
+apellido:"",
+edad:0,
+url:'http://daniesc.pythonanywhere.com/productos/'+id,
+}
+},
+methods: {
+fetchData(url) {
+fetch(url)
+.then(response => response.json())
+.then(data => {
+
+console.log(data)
+this.id=data.id
+this.nombre = data.nombre;
+this.imagen=data.imagen
+this.apellido=data.apellido
+this.edad=data.edad
+this.turno=data.turno
+})
+.catch(err => {
+console.error(err);
+this.error=true
+})
+},
+modificar() {
+let producto = {
+nombre:this.nombre,
+apellido: this.apellido,
+edad: this.edad,
+turno: this.turno,
+imagen:this.imagen
+}
+var options = {
+body: JSON.stringify(producto),
+method: 'PUT',
+headers: { 'Content-Type': 'application/json' },
+redirect: 'follow'
+}
+fetch(this.url, options)
+.then(function () {
+alert("Registro modificado")
+window.location.href = "/home/dani/Downloads/TFI/nuevo/templates/productos.html";
+})
+.catch(err => {
+console.error(err);
+alert("Error al modificar")
+})
+}
+},
+created() {
+this.fetchData(this.url)
+},
+}).mount('#app')
---
+++ productos.js
@@ -0,0 +1,72 @@
+const { createApp } = Vue
+createApp({
+data() {
+return {
+productos:[],
+//url:'http://localhost:5000/productos',
+// si el backend esta corriendo local usar localhost 5000(si no lo subieron a pythonanywhere)
+url:'http://daniesc.pythonanywhere.com/productos', // si ya lo subieron a pythonanywhere
+error:false,
+cargando:true,
+/*atributos para el guardar los valores del formulario */
+id:0,
+nombre:"",
+imagen:"",
+apellido:"",
+edad:0,
+turno:"",
+}
+},
+methods: {
+fetchData(url) {
+fetch(url)
+.then(response => response.json())
+.then(data => {
+this.productos = data;
+this.cargando=false
+})
+.catch(err => {
+console.error(err);
+this.error=true
+})
+},
+eliminar(producto) {
+const url = this.url+'/' + producto;
+var options = {
+method: 'DELETE',
+}
+fetch(url, options)
+.then(res => res.text()) // or res.json()
+.then(res => {
+location.reload();
+})
+},
+grabar(){
+let producto = {
+nombre:this.nombre,
+apellido: this.apellido,
+edad: this.edad,
+turno:this.turno,
+imagen:this.imagen
+}
+var options = {
+body:JSON.stringify(producto),
+method: 'POST',
+headers: { 'Content-Type': 'application/json' },
+redirect: 'follow'
+}
+fetch(this.url, options)
+.then(function () {
+alert("Registro grabado")
+window.location.href = "/home/dani/Downloads/TFI/nuevo/templates/productos.html";
+})
+.catch(err => {
+console.error(err);
+alert("Error al Guardar")
+})
+}
+},
+created() {
+this.fetchData(this.url)
+},
+}).mount('#app')
